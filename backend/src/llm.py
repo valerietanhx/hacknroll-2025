6107f1c3fd833aa43f1670b28c848019b19945b5
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2025-01-18 23:31:20 +0800

    add llm stuff

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -1,6 +1,14 @@
-def begin_conv(theme="fantasy"):
+from together import Together
+from dotenv import load_dotenv
+import os
+import re
+import json
+
+load_dotenv()
+
+def begin_conv(theme="fantasy") -> tuple[str, list]:
     """initial prompt to the llm to start the story"""
-    return [
+    conv_hist = [
         {
             "role": "system",
             "content": f"You are a story-teller for a text adventure game, which the user is going to play. \
@@ -10,6 +18,28 @@ def begin_conv(theme="fantasy"):
         }
     ]
 
+    client = Together()
+    response = client.chat.completions.create(
+        model=os.environ.get("LLM"),
+        messages=conv_hist,
+    )
+    output = response.choices[0].message.content
+    conv_hist.append({"role": "assistant", "content": output})
+    return output, conv_hist
+
+def generate_story(conversation_history: list, user_choice: str, stats: str) -> tuple[str, list]:
+    '''stats to be a string but in dictionary format, output of check_story'''
+    conversation_history.append({"role": "user", "content": user_choice})
+    analysis_history = conversation_history.copy()
+    analysis_history.append({"role": "user", "content": f"The stats of all relevant characters (including the main character, denoted as main, is as follows {stats})"})
+    client = Together()
+    response = client.chat.completions.create(
+        model=os.environ.get("LLM"),
+        messages=analysis_history,
+    )
+    output = response.choices[0].message.content
+    conversation_history.append({"role": "assistant", "content": output})
+    return output, conversation_history
 
 def check_story(conversation_history: list, curr_stats: dict):
     """check current stats and stuff"""
@@ -25,7 +55,7 @@ def check_story(conversation_history: list, curr_stats: dict):
 {curr_stats}
 
 Based on the latest update to the story, deduce whether a new character has been introduced and assign them a power level and health based on how strong you think they are. 
-Also deduce if the main character has lost or gained health/power. If a character is dead, the health should be set to 0.
+Also deduce if the main character (or "main" for short) has lost or gained health/power. If a character is dead, the health should be set to 0.
 Provide a summary of all updated stats in a code chunk. Here is an example:
 ```
 {"main": {"power": 500, "health": 100},  "goblin": {"power": 20, "health": 10}}
@@ -34,4 +64,17 @@ Provide a summary of all updated stats in a code chunk. Here is an example:
 """,
         }
     )
-    """Now we pass it through the llm and just extract the dict."""
+
+    client = Together()
+    response = client.chat.completions.create(
+        model=os.environ.get("LLM"),
+        messages=to_check,
+    )
+    # Now we pass it through the llm and just extract the dict as a string
+    return extract(response.choices[0].message.content)
+
+   
+
+def extract(message: str, pattern = r'^```(?:\w+)?\s*\n(.*?)(?=^```)```'):
+    '''Given a string, extract portions of the string within a code chunk demarcated by ``` symbols'''
+    return re.findall(pattern, message, re.DOTALL | re.MULTILINE)[0]
